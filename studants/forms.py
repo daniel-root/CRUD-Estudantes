d0884b63fc1e76de0e263a632e422bd3f74e5aa1
daniel-root <albuquerquedaniel898@gmail.com>
2020-09-02 11:28:58 -0300

    Models 'label'

--- studants/forms.py
+++ studants/forms.py
@@ -1,13 +1,11 @@
 from django import forms
 from .models import Studant
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit
-
 class StudantForm(forms.ModelForm):
     class Meta:
         model = Studant
         fields = '__all__'
 
         widgets = {
-                'birth_date': forms.TextInput(attrs={'type': 'date'}),
+            'birth_date': forms.TextInput(attrs={'type': 'date'}),
             }
+        
--- studants/models.py
+++ studants/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 from datetime import date
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+
 # Create your models here.
+def validate_even(value):
+    if int(value) % 2 != 0:
+        raise ValidationError(
+            _('%(value)s is not an even number'),
+            params={'value': value},
+        )
 class Studant(models.Model):
     COURSES = (
         ('Ciência da Computação', 'Ciência da Computação'),
@@ -14,7 +23,8 @@ class Studant(models.Model):
     birth_date = models.DateField('Data  de Nasimento')
     city = models.CharField('Cidade',max_length=50,default="Viçosa do Ceará")
     phone = models.CharField('Telefone',max_length=11)
-    cpf = models.CharField('CPF',max_length=11)
+    cpf = models.CharField('CPF',max_length=11,validators=[validate_even])
     course = models.CharField('Curso',max_length=50, choices=COURSES)
     email = models.EmailField('E-mail')
+    
     
